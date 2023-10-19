de27872ee7e955672421430bc6e02054ee876f41
shyun <c4521c@gmail.com>
2023-10-19 12:18:58 +0900

    10.19

---
+++ helloPhp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head></head>
+
+<body>
+    <?php
+    $hoge = 10;
+    echo($hoge);
+    $hoge = "Hello PHP!";
+    echo ($hoge);
+    ?>
+</body>
+
+</html>
---
+++ operation.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<?php
+    echo("PLUS:<br/>");
+    
+    $num = 2 + 4;
+    echo('$num = 2 + 4 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num = $num + 16;
+    echo('$num = $num + 16 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num += 100;
+    echo('$num += 100 => ');
+    echo($num);
+    echo("<br/>");
+
+    echo("<br/>");
+    echo("MINUS:<br/>");
+
+    $num = 2 + 4;
+    echo('$num = 2 - 4 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num = $num - 16;
+    echo('$num = $num - 16 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num -= 100;
+    echo('$num -= 100 => ');
+    echo($num);
+    echo("<br/>");
+    
+    echo("<br/>");
+    echo("MULTI:<br/>");
+
+    $num = 2 * 4;
+    echo('$num = 2 * 4 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num = $num * 16;
+    echo('$num = $num * 16 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num *= 100;
+    echo('$num *= 100 => ');
+    echo($num);
+    echo("<br/>");
+
+    echo("<br/>");
+    echo("DIVIS:<br/>");
+
+    $num = 2 / 4;
+    echo('$num = 2 / 4 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num = $num / 16;
+    echo('$num = $num / 16 => ');
+    echo($num);
+    echo("<br/>");
+
+    $num /= 100;
+    echo('$num /= 100 => ');
+    echo($num);
+    echo("<br/>");
+
+?>
